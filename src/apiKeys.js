904dd4437770c4bf7426da674f70322ff3bbf2df
BEERAM HARSHITH REDDY <208X1a05d2@khitguntur.ac.in>
2023-09-24 07:41:33 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "41e2f558595ef99a0b5996af2ddea380",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
